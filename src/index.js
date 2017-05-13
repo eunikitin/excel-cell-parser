43cb82e852f7b5cb05434542ef72fb86eb077b00
eunikitin <eu.nikitin@gmail.com>
2017-05-14 01:07:24 +0300

    Add parseCell function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import excelColumnName from 'excel-column-name';
 
+
+function getFirstDigitPosition(string) {
+  return string.search(/\d/);
+}
+
+export default function parseCell(cell) {
+  switch (typeof cell) {
+    case 'string':
+      return {
+        column: excelColumnName.excelColToInt(cell.slice(0, getFirstDigitPosition(cell))),
+        row: parseInt(cell.slice(getFirstDigitPosition(cell)), 10),
+      };
+    case 'object':
+      return excelColumnName.intToExcelCol(cell.column) + cell.row;
+    default:
+      throw new Error('Invalid argument type');
+  }
+}
--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -1,1 +1,14 @@
 import { expect } from 'chai';
+
+import parseCell from 'Src/index';
+
+
+describe('parseCell', () => {
+  it('should convert string to object', () => {
+    expect(parseCell('A1')).to.deep.equal({ column: 1, row: 1 });
+  });
+
+  it('should convert object to string', () => {
+    expect(parseCell({ column: 1, row: 1 })).to.deep.equal('A1');
+  });
+});
